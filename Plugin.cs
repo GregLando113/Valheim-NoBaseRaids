adceadb6c9178ff5ddf633675791c07637f4cb5a
Greg Landowski <greglando@gmail.com>
2022-12-06 23:19:50 -0600

    init

---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace NoBaseRaids
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static BepInEx.Logging.ManualLogSource MyLogger { get; private set; }
+
+        private readonly Harmony _harmony = new(PluginInfo.PLUGIN_GUID);
+        private void Awake()
+        {
+            MyLogger = Logger;
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+
+            _harmony.PatchAll();
+        }
+
+        private void OnDestroy()
+        {
+            _harmony.UnpatchSelf();
+        }
+
+    }
+}
---
+++ RaidPatcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using BepInEx;
+
+namespace NoBaseRaids
+{
+    [HarmonyPatch]
+    public class RaidPatcher
+    {
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(RandEventSystem), "UpdateRandomEvent")]
+        static bool UpdateRandomEventHook()
+        {
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(RandEventSystem), "UpdateForcedEvents")]
+        static bool UpdateForcedEventHook()
+        {
+            return false;
+        }
+    }
+}
