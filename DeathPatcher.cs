69e43fda032c5f571895899ef79085c2da587f27
Greg Landowski <greglando@gmail.com>
2022-12-21 20:14:07 -0600

    add death/tp stop

---
+++ DeathPatcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx;
+
+namespace NoBaseRaids
+{
+    [HarmonyPatch]
+    public class DeathPatcher
+    {
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Player), "CreateTombStone")]
+        static bool CreateTombStoneHook()
+        {
+            return false;
+        }
+    }
+}
---
+++ TeleportPatcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx;
+
+namespace NoBaseRaids
+{
+    [HarmonyPatch]
+    public class TeleportPatcher
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Inventory), "IsTeleportable")]
+        static bool IsTeleportableHook(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
